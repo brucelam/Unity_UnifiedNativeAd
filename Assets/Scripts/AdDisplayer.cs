51c82e7dab47e4e9b20a277a97b67301e748df60
Bruce Lam <brucelam21@gmail.com>
2019-09-26 09:51:14 +0700

    first commit

---
+++ Assets/Scripts/AdDisplayer.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+#if EASY_MOBILE
+using EasyMobile;
+#endif
+public enum GameState
+{
+    Prepare,
+    Playing,
+    Paused,
+    PreGameOver,
+    GameOver
+}
+
+public class AdDisplayer : MonoBehaviour
+{
+    public static string get_unit_id_reward()
+    {
+#if UNITY_ANDROID
+        return app_id_android_rw;
+#endif
+        return app_id_ios_rw;
+    }
+    public static string get_unit_id_inter()
+    {
+#if UNITY_ANDROID
+        return app_id_android_inter;
+#endif
+        return app_id_ios_inter;
+    }
+    public static string get_unit_id_native()
+    {
+#if UNITY_ANDROID
+        return app_id_android_native;
+#endif
+        return app_id_ios_native;
+    }
+    public static string get_unit_id_banner()
+    {
+#if UNITY_ANDROID
+        return app_id_android_banner;
+#endif
+        return app_id_ios_banner;
+    }
+    public static string get_app_id()
+    {
+#if UNITY_ANDROID
+        return app_id_android;
+#endif
+        return app_id_ios;
+    }
+
+    public const string app_id_ios = "ca-app-pub-4585119275656037~3658295529";
+    public const string app_id_android = "ca-app-pub-4585119275656037~2486921834";
+
+    public const string app_id_ios_banner = "ca-app-pub-4585119275656037/3801943360";
+    public const string app_id_ios_inter = "ca-app-pub-4585119275656037/3466723839";
+    public const string app_id_ios_native = "ca-app-pub-4585119275656037/7945076071";
+    public const string app_id_ios_rw = "ca-app-pub-4585119275656037/3742233611";
+
+    public const string app_id_android_banner = "ca-app-pub-4585119275656037/4472164763";
+    public const string app_id_android_inter = "ca-app-pub-4585119275656037/8669186805";
+    public const string app_id_android_native = "ca-app-pub-4585119275656037/2306519438";
+    public const string app_id_android_rw = "ca-app-pub-4585119275656037/2384853557";
+
+
+    public static AdDisplayer Instance { get; private set; }
+    void Awake()
+    {
+
+        if (Instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+    void init()
+    {
+
+        string appId = get_app_id();
+
+        MobileAds.SetiOSAppPauseOnBackground(true);
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+    }
+    public void Start()
+    {
+        init();
+    }
+}
+
---
+++ Assets/Scripts/Ads_Native.cs
@@ -0,0 +1,293 @@
+﻿using GoogleMobileAds.Api;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ads_Native : MonoBehaviour
+{
+    private void RequestNativeAd()
+    {
+            var unitId = AdDisplayer.get_unit_id_native();
+            AdLoader adLoader = new AdLoader.Builder(unitId)
+                .ForUnifiedNativeAd()
+                .Build();
+            adLoader.OnUnifiedNativeAdLoaded += HandleUnifiedNativeAdLoaded;
+            adLoader.OnAdFailedToLoad += AdLoader_OnAdFailedToLoad;
+            var rq = new AdRequest.Builder()
+                .AddTestDevice(AdRequest.TestDeviceSimulator)
+                .AddTestDevice("954EEF7C5310E3782526624C14668FCA")
+                .Build();
+            adLoader.LoadAd(rq);
+
+        
+    }
+
+
+
+
+    void init()
+    {
+        RequestNativeAd();
+        gameObject.SetActive(false);
+        if (Application.isEditor)
+        {
+            gameObject.SetActive(true);
+            double stars = 3.5;
+            check_rate(stars);
+        }
+    }
+
+    private void Start()
+    {
+        init();
+    }
+    void check_rate(double stars)
+    {
+        if (stars >= 0)
+        {
+            rateText.text = stars + "";
+            rateText.gameObject.SetActive(true);
+            starRating.SetActive(true);
+            Store.gameObject.SetActive(true);
+            //BodyText.gameObject.SetActive(false);
+            for (int i = 0; i < 5; i++)
+            {
+                var star = this.starRating.transform.GetChild(i).gameObject;
+                star.SetActive(false);
+            }
+            if (stars > 1) { this.starRating.transform.GetChild(0).gameObject.SetActive(true); }
+            if (stars > 2) { this.starRating.transform.GetChild(1).gameObject.SetActive(true); }
+            if (stars > 3) { this.starRating.transform.GetChild(2).gameObject.SetActive(true); }
+            if (stars > 4 && stars < 5) { this.starRating.transform.GetChild(3).gameObject.SetActive(true); }
+            if (stars >= 5) { this.starRating.transform.GetChild(4).gameObject.SetActive(true); }
+        }
+        else
+        {
+            rateText.gameObject.SetActive(false);
+            starRating.SetActive(false);
+            Store.gameObject.SetActive(false);
+            //BodyText.gameObject.SetActive(true);
+        }
+    }
+    void set_ad()
+    {
+        var ad_choise_Texture = nativeAd.GetAdChoicesLogoTexture();
+        if (ad_choise_Texture != null)
+        {
+
+            AdChoices.texture = ad_choise_Texture;
+            if (!nativeAd.RegisterAdChoicesLogoGameObject(AdChoices.gameObject))
+            {
+                Debug.Log("***************RegisterAdChoicesLogoGameObject failed!");
+            }
+        }
+        else
+            AdChoices.gameObject.SetActive(false);
+        string headline = nativeAd.GetHeadlineText();
+        if (!string.IsNullOrEmpty(headline))
+        {
+            HeadlineText.text = headline;
+            if (!nativeAd.RegisterHeadlineTextGameObject(HeadlineText.gameObject))
+            {
+                Debug.Log("***************RegisterHeadlineTextGameObject failed!");
+            }
+        }
+        else
+        {
+            HeadlineText.gameObject.SetActive(false);
+        }
+        string body = nativeAd.GetBodyText();
+        if (!string.IsNullOrEmpty(headline))
+        {
+            BodyText.text = body;
+            if (!nativeAd.RegisterBodyTextGameObject(BodyText.gameObject))
+            {
+                Debug.Log("***************RegisterBodyTextGameObject failed!");
+            }
+        }
+        else
+        {
+            BodyText.gameObject.SetActive(false);
+        }
+
+
+        List<Texture2D> imgs = nativeAd.GetImageTextures();
+        if (imgs != null && imgs.Count > 0)
+        {
+
+            bigImage.GetComponent<RawImage>().texture = imgs[0];
+            List<GameObject> imageObjs = new List<GameObject>();
+            imageObjs.Add(bigImage.gameObject);
+            nativeAd.RegisterImageGameObjects(imageObjs);
+        }
+        if (con != null)
+        {
+            if (!nativeAd.RegisterCallToActionGameObject(con))
+            {
+                Debug.Log("***************RegisterCallToActionGameObject failed!");
+            }
+        }
+
+        string call_to = nativeAd.GetCallToActionText();
+
+        if (!string.IsNullOrEmpty(call_to))
+        {
+            //CallToAction.AddComponent<BoxCollider2D>();
+            CallToAction.text = call_to;
+            //CallToAction.gameObject.AddComponent<BoxCollider>();
+            if (!nativeAd.RegisterCallToActionGameObject(CallToAction.gameObject))
+            {
+                Debug.Log("***************RegisterCallToActionGameObject failed!");
+            }
+        }
+        else
+        {
+            CallToAction.gameObject.SetActive(false);
+        }
+
+        double stars = nativeAd.GetStarRating();
+
+        check_rate(stars);
+
+        Texture2D iconTexture = nativeAd.GetIconTexture();
+        if (iconTexture != null)
+        {
+
+            IconImage.texture = iconTexture;
+
+            if (!nativeAd.RegisterIconImageGameObject(IconImage.gameObject))
+            {
+                Debug.Log("***************RegisterIconImageGameObject failed!");
+            }
+        }
+        else
+            IconImage.gameObject.SetActive(false);
+        string store = nativeAd.GetStore();
+
+        if (!string.IsNullOrEmpty(headline))
+        {
+            Store.text = store;
+            if (!nativeAd.RegisterStoreGameObject(Store.gameObject))
+            {
+                Debug.Log("***************RegisterStoreGameObject failed!");
+            }
+        }
+        else
+        {
+            Store.gameObject.SetActive(false);
+        }
+
+        string price = nativeAd.GetPrice();
+
+        if (!string.IsNullOrEmpty(headline))
+        {
+
+            Price.text = price;
+            if (!nativeAd.RegisterPriceGameObject(Price.gameObject))
+            {
+                Debug.Log("***************RegisterPriceGameObject failed!");
+            }
+        }
+        else
+        {
+            Price.gameObject.SetActive(false);
+        }
+        string adv_text = nativeAd.GetAdvertiserText();
+        if (!string.IsNullOrEmpty(adv_text))
+        {
+            AdvertiserText.text = adv_text;
+            if (!nativeAd.RegisterAdvertiserTextGameObject(AdvertiserText.gameObject))
+            {
+                Debug.Log("***************RegisterAdvertiserTextGameObject failed!");
+            }
+        }
+        else
+        {
+            AdvertiserText.gameObject.SetActive(false);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    private void AdLoader_OnAdFailedToLoad(object sender, AdFailedToLoadEventArgs e)
+    {
+        Debug.Log("***********AdLoader_OnAdFailedToLoad : " + e.Message);
+    }
+
+    private void HandleUnifiedNativeAdLoaded(object sender, UnifiedNativeAdEventArgs args)
+    {
+        gameObject.SetActive(true);
+        //MonoBehaviour.print("Unified native ad loaded.");
+        nativeAd = args.nativeAd;
+        unifiedNativeAdLoaded = true;
+        //set_ad();
+        //Debug.Log("***********HandleUnifiedNativeAdLoaded : ");
+
+
+    }
+    private bool unifiedNativeAdLoaded;
+    private UnifiedNativeAd nativeAd;
+    public RawImage AdChoices, IconImage, bigImage;
+    public Text AdvertiserText, BodyText, CallToAction, HeadlineText, Price, Store, rateText;
+    public GameObject starRating, con;
+    void Update()
+    {
+
+        if (unifiedNativeAdLoaded)
+        {
+
+            unifiedNativeAdLoaded = false;
+
+            set_ad();
+
+
+        }
+    }
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += LogCallback;
+    }
+
+    //Called when there is an exception
+    void LogCallback(string condition, string stackTrace, LogType type)
+    {
+        
+        Debug.Log("******l4m: type: " + type + "-----stackTrace" + stackTrace + "-----condition" + condition);
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= LogCallback;
+    }
+}
